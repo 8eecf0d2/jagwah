c6ffe0986a54562a123be8ef1beab6ef5e6dd27a
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-06-27 18:34:58 +1000

    chore: added todo's

--- src/main/ts/helpers/http.ts
+++ src/main/ts/helpers/http.ts
@@ -2,6 +2,12 @@
  * hyperbole by 8eecf0d2
  */
 
+/**
+ * TODO: currently only intended for json requests,
+ * probably best to refactor into a more capable
+ * http helper class with different methods and
+ * whatnot..
+ */
 export async function http(method: string, url: string, body?: any): Promise<IHTTPData> {
 	let result: IHTTPData;
 	await fetch(url,
--- src/main/ts/helpers/sync.ts
+++ src/main/ts/helpers/sync.ts
@@ -2,6 +2,11 @@
  * hyperbole by 8eecf0d2
  */
 
+/**
+ * TODO: possibly extends for 2 way binding...
+ * not sure if I like the idea of two way binding
+ * or not... maybe leverage proxies(?)
+ */
 export function sync(obj: any, prop: string) {
 	return (el: any) => {
 		obj[prop] = el.target.value;
