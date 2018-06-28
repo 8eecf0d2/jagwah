945d2e2c7c98452b4e96f4a573c12dc9e360876c
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-06-28 12:41:56 +1000

    chore: updated to use `@Templates`

--- example/demo-site/src/ts/routes/main.ts
+++ example/demo-site/src/ts/routes/main.ts
@@ -2,17 +2,13 @@
  * 8eecf0d2/hyperbole - 2018
  */
 
-import { Hyperbole, Route, Filters, Helpers } from 'hyperbole';
+import { Hyperbole, Route, Templates } from 'hyperbole';
 import { LandingTemplate, GettingStartedTemplate } from '../templates';
 
 @Route('/')
-export class LandingRoute {
-	static $templates = [LandingTemplate]
-	constructor(){}
-}
+@Templates([LandingTemplate])
+export class LandingRoute {}
 
 @Route('/getting-started')
-export class GettingStartedRoute {
-	static $templates = [GettingStartedTemplate]
-	constructor(){}
-}
+@Templates([GettingStartedTemplate])
+export class GettingStartedRoute {}
