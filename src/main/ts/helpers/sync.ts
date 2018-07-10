8d538112e94552dba2a8a8f7eb195e9d97234c7c
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-07-10 17:54:00 +1000

    feat: added `safeSet` to sync helper (will be removed entirely soon)

--- src/main/ts/helpers/sync.ts
+++ src/main/ts/helpers/sync.ts
@@ -2,13 +2,15 @@
  * hyperbole by 8eecf0d2
  */
 
+import { safeSet } from './safe-set';
+
 /**
  * TODO: possibly extends for 2 way binding...
  * not sure if I like the idea of two way binding
  * or not... maybe leverage proxies(?)
  */
-export function sync(obj: any, prop: string) {
+export function sync(obj: any, path: string) {
 	return (el: any) => {
-		obj[prop] = el.target.value;
+		safeSet(obj, path, el.target.value);
 	}
 }
