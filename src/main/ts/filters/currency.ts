46ee80b8f5a19df44d02641289fec1359b82aeea
Brod <8eecf0d23d248b0e6223b8a10ddbf1@gmail.com>
2018-07-11 14:08:49 +1000

    feat: removed filters source

--- src/main/ts/filters/currency.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Hyperbolé - https://github.com/8eecf0d2/hyperbole
- */
-
-export function currency(value: number, symbol: string = '$') {
-	return `${symbol}${value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')}`;
-}
